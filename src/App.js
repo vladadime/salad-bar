12542f438b4b44fd61c5a94d1cdcd1a68f1adb85
vladadime <dime.97@hotmail.com>
2023-10-01 00:45:51 +0200

    Adding routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import {Layout} from './components';
+import {AdminPanel, Home} from './pages';
 
 function App() {
     return (
         <div className="App">
-            <Layout/>
+            <Layout>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={< Home />}/>
+                        <Route path="/admin" element={< AdminPanel />}/>
+                    </Routes>
+                </BrowserRouter>
+            </Layout>
         </div>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export {default as AdminPanel}
 from './AdminPanel/AdminPanel';
 export {default as Home}
-from './Home/Home';
+from './HomePage/Home';
