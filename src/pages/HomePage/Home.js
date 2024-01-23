4e952ec236b1cd989bc261c5e8ce010a79af3ad9
vladadime <dime.97@hotmail.com>
2024-01-23 22:55:46 +0100

    Revert "Revert "Code correction in the ModalDialog component""
    This reverts commit af27eadc063306259a7509386015757080543c14.

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,7 +1,112 @@
+import Context from '../../contexts/ContextProvider'
+import ModalDialog from '../../components/ModalDialog/ModalDialog'
+import { useContext } from 'react'
+import { Pagination } from '../../components'
+import Table from '../../components/Table/Table'
+
+const columns = [
+  {
+    field: 'name',
+    headerName: 'Name',
+    sortable: true,
+  },
+  {
+    field: 'calories',
+    headerName: 'Calories',
+    sortable: true,
+  },
+  {
+    field: 'tag',
+    headerName: 'Tag',
+  },
+];
+
+const ingredientColumns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      sortable: true,
+    },
+    {
+      field: 'image',
+      headerName: 'Image',
+    },
+    {
+      field: 'calories',
+      headerName: 'Calories',
+      sortable: true,
+    },
+    {
+      field: 'tag',
+      headerName: 'Tag',
+    },
+];
+
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
+  const {
+    activeModal,
+    currentPage,
+    ingredients,
+    filterSearch,
+    modalDisplay,
+    modalToggle,
+    salads,
+    setActiveModal,
+    sortSalads,
+  } = useContext(Context)
+  const perPage = 5
+  return (
+    <div className='container-fluid'>
+      <div className='col-9 col-md-10 table-responsive'></div>
+      <ModalDialog
+        isOpen={modalDisplay}
+        onClose={modalToggle}
+        modalContent={activeModal}
+      />
+      <div className='d-flex flex-row justify-content-center mt-5'>
+        <div className='col-4 col-md-3 mt-5'>
+          <button
+            className='btn btn-primary'
+            type='submit'
+            onClick={() => {
+              modalToggle(true)
+              setActiveModal({ type: 'addSalad' })
+            }}
+          >
+            Add new salad
+          </button>
+        </div>
+        <div className='col-6 col-md-5 mt-5'>
+          <div className='d-flex'>
+            <input
+              className='form-control'
+              type='search'
+              placeholder='Enter tag name'
+              aria-label='Search'
+              onChange={filterSearch}
+            />
+          </div>
+        </div>
+      </div>
+      <Table
+        headCols={columns}
+        data={salads}
+        perPage={perPage}
+        actions={{
+          delete: 'deleteSalad',
+          edit: 'editSalad',
+        }}
+        sort={sortSalads}
+      />
+      {/* {salads.length && (
+        <Pagination
+          currentPage={currentPage}
+          totalRecords={salads.length}
+          perPage={perPage}
+        />
+      )} */}
+    </div>
+  )
 }
 
-export default Home;
+export default Home
