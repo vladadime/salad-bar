d2f16808d4cabd35ba9b8158cde2fde423ec09e9
vladadime <dime.97@hotmail.com>
2024-01-24 23:50:37 +0100

    Prettify

--- src/contexts/Reducer.js
+++ src/contexts/Reducer.js
@@ -1,88 +1,85 @@
 import {
-    ACTIVE_MODAL,
-    ADD_INGREDIENT,
-    DELETE_INGREDIENT,
-    EDIT_INGREDIENT,
-    FILTER_SEARCH,
-    FILTER_TOGGLE,
-    GET_INGREDIENTS,
-    MODAL_TOGGLE,
-    NAVIGATE,
-    SORT_INGREDIENTS,
-    SORT_TOGGLE
-} from "./types";
+  ACTIVE_MODAL,
+  ADD_INGREDIENT,
+  DELETE_INGREDIENT,
+  EDIT_INGREDIENT,
+  FILTER_SEARCH,
+  FILTER_TOGGLE,
+  GET_INGREDIENTS,
+  MODAL_TOGGLE,
+  NAVIGATE,
+  SORT_INGREDIENTS,
+  SORT_TOGGLE,
+} from './types'
 
-const ingredientReducer = (state, {type, payload}) => {
-    switch (type) {
-        case ACTIVE_MODAL:
-            return {
-                ...state,
-                activeModal: payload
-            };
-        case ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: [
-                    ...state.ingredients,
-                    payload
-                ]
-            };
-        case DELETE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: state
-                    .ingredients
-                    .filter((ingredient) => ingredient.id !== payload)
-            };
-        case EDIT_INGREDIENT:
-            return {
-                ...state,
-                ingredients: state
-                    .ingredients
-                    .map((item) => item.id === payload.id
-                        ? {
-                            ...item,
-                            ...payload
-                        }
-                        : item)
-            };
-        case FILTER_TOGGLE:
-            return {
-                ...state,
-                filters: payload
-            };
-        case FILTER_SEARCH:
-            return {
-                ...state,
-                searchText: payload
-            };
-        case GET_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: payload
-            };
-        case MODAL_TOGGLE:
-            return {
-                ...state,
-                modalDisplay: payload
-            };
-        case NAVIGATE:
-            return {
-                ...state,
-                currentPage: payload
-            };
-        case SORT_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: payload
-            };
-        case SORT_TOGGLE:
-            return {
-                ...state,
-                order: payload
-            }
-        default:
-            return state;
-    }
+const ingredientReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ACTIVE_MODAL:
+      return {
+        ...state,
+        activeModal: payload,
+      }
+    case ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, payload],
+      }
+    case DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter(
+          (ingredient) => ingredient.id !== payload,
+        ),
+      }
+    case EDIT_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.map((item) =>
+          item.id === payload.id
+            ? {
+                ...item,
+                ...payload,
+              }
+            : item,
+        ),
+      }
+    case FILTER_TOGGLE:
+      return {
+        ...state,
+        filters: payload,
+      }
+    case FILTER_SEARCH:
+      return {
+        ...state,
+        searchText: payload,
+      }
+    case GET_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: payload,
+      }
+    case MODAL_TOGGLE:
+      return {
+        ...state,
+        modalDisplay: payload,
+      }
+    case NAVIGATE:
+      return {
+        ...state,
+        currentPage: payload,
+      }
+    case SORT_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: payload,
+      }
+    case SORT_TOGGLE:
+      return {
+        ...state,
+        order: payload,
+      }
+    default:
+      return state
+  }
 }
-export default ingredientReducer;
+export default ingredientReducer
